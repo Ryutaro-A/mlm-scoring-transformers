2cbb1e614579e06b7555b475ee582aa7862668f1
mizuki@kanolab <trombonelover8888@gmail.com>
2023-07-09 17:16:25 +0900

    [fix] can use 'import mlmt' from other file

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
+from glob import glob
+from os.path import basename
+from os.path import splitext
 
 setup(
-    name='mlm-scoring-transformers',
-    version='0.1',
+    name="mlm-scoring-transformers",
+    version="0.1",
     description="Masked Language Model Scoring by Huggingface transformers",
     author="Ryutaro Asahara",
-    packages=find_packages("mlmt"),
-    install_requires=[
-        'transformers',
-        'fugashi<=1.1.2',
-        'ipadic<=1.0.0'
-    ],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    install_requires=["transformers", "fugashi<=1.1.2", "ipadic<=1.0.0"],
 )
+
